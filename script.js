ff8f3a0a7179e328af74c14e782f992a4336c2e8
Darya Kotova <dashakotova0702@gmail.com>
2021-10-14 23:21:17 +0300

    work for pc

--- script.js
+++ script.js
@@ -6,3 +6,20 @@ $(window).scroll(function(){
     cloneHeader.addClass('header_is_show');
   }
 });
+
+document.querySelectorAll('.anchor').forEach(link => {
+  link.addEventListener('click', function (e){
+    console.log('click')
+    e.preventDefault()
+    const href = this.getAttribute('href').substring(1)
+    const scrollTarget = document.getElementById(href)
+    const topOffset = 70
+    const elementPosition = scrollTarget.getBoundingClientRect().top
+    const offsetPosition = elementPosition - topOffset
+
+    window.scrollBy ({
+      top: offsetPosition,
+      behavior: 'smooth',
+    })
+  })
+})
